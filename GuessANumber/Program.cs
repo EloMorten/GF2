4b2bfac9c786d2192d0717fd08436736b95d9062
EloMorten <mort736g@gmail.com>
2026-09-03 13:28:28 +0200

    Update Program.cs

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -1,7 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net.Security;
+
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Gæt et tal, please!");
 
 int guess = int.Parse(Console.ReadLine());
 
 Console.WriteLine(guess);
-
+Console.WriteLine("hello!");

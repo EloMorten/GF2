918d7cd5cfd2783a9099522cb891904b8141f369
EloMorten <mort736g@gmail.com>
2026-09-03 22:25:44 +0200

    Update Program.cs

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System.Net.Security;
 
-Console.WriteLine("Hello, World!");
 Random rand = new Random();
-int tal = rand.Next(1, 11);
-Console.WriteLine(tal);
+int tal = rand.Next(1, 101);
+Console.WriteLine("gæt et tal mellem 1 og 100");
 int number = int.Parse(Console.ReadLine());
 
 while (number != tal)
 {
     if (number > tal)
     {
-        Console.WriteLine("lower");
+        Console.WriteLine("lower!");
     }
     else if (number < tal)
     {
         Console.WriteLine("higer!");
     }
-    else if (number == tal)
-    {
-        Console.WriteLine("flot!");
-    }
+
+    number = int.Parse(Console.ReadLine());
 }
+Console.WriteLine("flot!");
